603766825695a044ae6b9eb392b79cb19538638f
fight-team-devs <57288397+fight-team-devs@users.noreply.github.com>
2019-12-08 20:59:35 +0700

    nguyen hai check buoi dau

---
+++ chuong2/chuong2/Source.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int a, b;
+	cout << "nhap so tu nhien a:";
+	cin >> a;
+	cout << "a=" << a << endl;
+	cout << "nhap so tu nhien b:";
+	cin >> b;
+	cout << "b=" << b;
+	return 0;
+}
