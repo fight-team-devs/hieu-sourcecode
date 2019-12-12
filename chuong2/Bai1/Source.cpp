f1fd00a1232cb68384a72ada4a38fbbe0f810381
fight-team-devs <57288397+fight-team-devs@users.noreply.github.com>
2019-12-12 09:09:17 +0700

    oke

--- chuong2/Bai1/Source.cpp
+++ chuong2/Bai1/Source.cpp
@@ -1,14 +1,25 @@
-#include <iostream>
+﻿#include <iostream>
 using namespace std;
 
 int main()
 {
-	int a, b;
+	int a, b, c,d,soluong,dongia,tien;
 	cout << "nhap so tu nhien a:";
 	cin >> a;
-	cout << "a=" << a << endl;
+	cout<< endl;
 	cout << "nhap so tu nhien b:";
 	cin >> b;
-	cout << "b=" << b;
+	cout <<endl;
+	c = a + b;
+	cout << "tong cua a va b:"<<c<<endl;
+	d = c - a;
+	cout << "hieu cua hai so a va c:"<<d<<endl;
+	cout << "nhap so luong:";
+	cin >> soluong;
+	cout << endl;
+	
+	cout << "nhap vao don gia:"<< "D";
+
+	cin >> dongia;
 	return 0;
 }
